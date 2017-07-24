7d7af396e8fb2c04279578f8ac49ff6ae1c8e8f5
ardeleanasm <ardeleanasm@gmail.com>
2017-07-24 23:11:25 +0300

    tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,35 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+module Main where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+
+
+--test1 = TestCase (assertEqual "for (foo 3)," (1,2) (foo 3))
+test1::TestTree
+test1 = testCase "Dummy test"
+    (assertEqual "for (foo 3)," (1,2) (1,2))
+
+main = do
+  defaultMain (testGroup "Our Library Tests" [test1])
+
+
+--module Main where
+
+--import Test.Tasty
+--import Test.Tasty.HUnit
+
+--import Lib
+--import MathLib
+
+--main :: IO ()
+--main = do
+--  defaultMain (testGroup "Our Library Tests" [sayYoTest, add5Test])
+
+--sayYoTest :: TestTree
+--sayYoTest = testCase "Testing sayYo"
+--  (assertEqual "Should say Yo to Friend!" "Yo Friend!" (sayYo "Friend"))
+
+--add5Test :: TestTree
+--add5Test = testCase "Testing add5"
+--  (assertEqual "Should add 5 to get 10" 10 (add5 5))
