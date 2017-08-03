d2d3c3bfbcd47d72162afad50ecd77c1831ce261
ardeleanasm <ardeleanasm@gmail.com>
2017-08-03 23:29:03 +0300

    updated doc

--- copy_doc.sh
+++ copy_doc.sh
@@ -5,6 +5,6 @@ rm docs/*.*
 rm -r docs/src
 
 #copy new documentation
-cp -r .stack-work/install/x86_64-linux-nopie/lts-8.23/8.0.2/doc/qchas-0.1.0.0/*.* docs/
+cp -r .stack-work/install/x86_64-linux-nopie/lts-8.23/8.0.2/doc/qchas-1.0.0/*.* docs/
 mkdir docs/src
-cp -r .stack-work/install/x86_64-linux-nopie/lts-8.23/8.0.2/doc/qchas-0.1.0.0/src/*.* docs/src
+cp -r .stack-work/install/x86_64-linux-nopie/lts-8.23/8.0.2/doc/qchas-1.0.0/src/*.* docs/src
